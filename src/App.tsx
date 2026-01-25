1140023491b567a9155d01a293ed6d8f8437fca5
Pedro Andreoli <114033109+PedroVictorAndreoli@users.noreply.github.com>
2026-01-25 18:32:21 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import './playerVideo.css';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
